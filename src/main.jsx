1bde99ac3b1b0ff15f3a7af0dd8d45ed425f1afe
Bryan <93453907+blopez024@users.noreply.github.com>
2025-07-29 18:46:28 -0700

    finish pokemon details page setup

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.jsx';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import PokemonDetails from './PokemonDetails.jsx';
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon/:pokemonName" element={<PokemonDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
+);
